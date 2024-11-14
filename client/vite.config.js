14ceffee74c214bd55b5865b44b4abf257d3a218
Muhammad Sami <mirzasamibaig17@gmail.com>
2024-11-14 15:17:11 +0500

    small changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], build: {
-    outDir: 'dist' // Vercel expects 'dist' as the default output
+  plugins: [react()],
+  root: 'client', // Update this if your files are in a "client" directory
+  build: {
+    outDir: 'dist' // Ensure that the output directory is correct for Vercel
   }
 })
